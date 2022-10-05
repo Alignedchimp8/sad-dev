3b4b875626c154416535f9a5ff550e144cfd4611
thedev012 <115072272+thedev012@users.noreply.github.com>
2022-10-05 15:00:44 +0530

    changed something
    hello
    
    Co-Authored-By: Sam Sharma <72144419+javascript-spec@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,10 +1,6 @@
-// hey this is a comment
-console.log("Hello World")
-console.log("Hello World") // 2nd time!
+console.log("Hello World");
 
 function main() {
-return null;
+  console.log(1);
 }
-main()
-
-// console.log(main)
+main();
